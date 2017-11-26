811b113f0ce5fe42db7a3058871b2387556c4eab
Brandon Yapel <brandonyapel@gmail.com>
2017-11-26 16:43:33 -0600

    completed add listItem, started submitChecks

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,10 +12,17 @@ function readyNow() {
 
     //event listeners
     $('main').on('click', '.listDiv', listDivClick);
+    $('main').on('click', '.deleteListItem', deleteListItem);
+    $('main').on('click', '.addListItemButton', addListItem);
+    $('main').on('click', '.insertListItem', insertListItem);
+    $('main').on('click', '.cancelListItem', cancelListItem);
+    $('main').on('click', '.submitChecks', submitChecks)
 };
 
 //global variables for functions
-var currentTable = { is: 'N/A', columnNames: [], tableData: [] };
+var currentTable = { is: '', columnNames: [], tableData: [], newItem: [] };
+var checkbox;
+var deleteButton;
 
 
 
@@ -51,9 +58,10 @@ function getAllLists() {
 }//end getAllLists()
 
 function listDivClick() {
-    currentTable.is = $(this).find($('h2')).text();
     listDivHeight = $(this).height()
     if (listDivHeight == 200) {
+        refreshBoxes();
+        currentTable.is = $(this).find($('h2')).text();
         $('.listDiv').animate({
             height: "200px",
             width: "200px"
@@ -63,6 +71,7 @@ function listDivClick() {
             height: "+=440px",
             width: "+=440px"
         });//end animate
+        refreshBoxes
         postCurrentTableForColumns();
         //ajax request to get column names of table
     }
@@ -124,30 +133,130 @@ function getTableData() {
 }
 
 function appendTableTooListDiv() {
+    refreshBoxes();
     var table = '<table id = "table' + currentTable.is + '"></table>'
     var thead = '<thead id = "thead' + currentTable.is + '"></thead>'
     var tbody = '<tbody id = "tbody' + currentTable.is + '"></tbody>'
     $('#div' + currentTable.is).append(table);
     $('#table' + currentTable.is).append(thead);
     $('#table' + currentTable.is).append(tbody);
-    theadtr= '<tr id = "theadtr'+currentTable.is+'">'+'</tr>';
+    theadtr = '<tr id = "theadtr' + currentTable.is + '">' + '</tr>';
     $('#thead' + currentTable.is).append(theadtr)
-    for (let thIndex = 1; thIndex < currentTable.columnNames.length; thIndex++) {
+    $('#theadtr' + currentTable.is).append('<th>✓</th>');
+    for (let thIndex = 2; thIndex < currentTable.columnNames.length; thIndex++) {
         var theadAppendItem = '';
         theadAppendItem += '<th>' + currentTable.columnNames[thIndex] + '</th>'
-        $('#theadtr'+currentTable.is).append(theadAppendItem);
+        $('#theadtr' + currentTable.is).append('<th>' + currentTable.columnNames[thIndex] + '</th>');
     };
+    $('#theadtr' + currentTable.is).append('<th>x</th>');
     for (let trIndex = 0; trIndex < currentTable.tableData.length; trIndex++) {
         var tbodyAppendItem = '';
         tbodyAppendItem += '<tr id = "tr' + currentTable.is + currentTable.tableData[trIndex].id + '"></tr>';
-        $('#tbody'+currentTable.is).append(tbodyAppendItem);
-        for (let columnNameIndex = 1; columnNameIndex < currentTable.columnNames.length; columnNameIndex++) {
+        $('#tbody' + currentTable.is).append(tbodyAppendItem);
+        checkbox = '<td><input class = "checkBox" id="checkBox' + currentTable.is + currentTable.tableData[trIndex].id + '" type="checkbox"></td>'
+        $('#tr' + currentTable.is + currentTable.tableData[trIndex].id).append(checkbox)
+        for (let columnNameIndex = 2; columnNameIndex < currentTable.columnNames.length; columnNameIndex++) {
             var currentColumn = currentTable.columnNames[columnNameIndex];
             console.log(currentColumn);
             var trAppendItem = '';
             trAppendItem += '<td>' + currentTable.tableData[trIndex][currentColumn] + '</td>'
             $('#tr' + currentTable.is + currentTable.tableData[trIndex].id).append(trAppendItem);
         }
-
+        deleteButton = '<td><button data-id=' + currentTable.tableData[trIndex].id + ' class="deleteListItem" id=delete' + currentTable.is + currentTable.tableData[trIndex].id + '>x</button></td>'
+        $('#tr' + currentTable.is + currentTable.tableData[trIndex].id).append(deleteButton);
     }
+    //apend table footer
+    var columnSpan = currentTable.columnNames.length;
+    var tableFooter = ''
+    tableFooter += '<tfoot>';
+    tableFooter += '<tr>';
+    tableFooter += '<td colspan="' + columnSpan + '">';
+    tableFooter += '<button class = "addListItemButton" id = "addItem' + currentTable.is + '">add to list</button>'
+    tableFooter += '<button class = "submitChecks">submit checks</button>';
+    tableFooter += '<button>delete list</button>';
+    tableFooter += '<button>minimize list</button></td>';
+    tableFooter += '</tr>';
+    tableFooter += '<tfoot>';
+    $('#table' + currentTable.is).append(tableFooter);
+};
+
+function refreshBoxes() {
+    $('#table' + currentTable.is).remove();
+};
+
+function deleteListItem() {
+    console.log($(this).data());
+    var listItemToRemove = $(this).data().id;
+    console.log('deleteListItem was clicked! The list item id was', listItemToRemove);
+
+    $.ajax({
+        method: 'DELETE',
+        url: '/list/' + listItemToRemove,
+        success: function (response) {
+            refreshBoxes();
+            getTableData();
+        }
+    });
 }
+
+function addListItem() {
+    $('tfoot').remove()
+    var colspan = currentTable.columnNames.length;
+    var addListAppendItem = '';
+    addListAppendItem += '<tr>'
+    addListAppendItem += '<td colspan = "' + colspan + '">'
+    addListAppendItem += 'Add new item below'
+    addListAppendItem += '</td>'
+    addListAppendItem += '</tr>'
+    addListAppendItem += '<tr>'
+    addListAppendItem += '<td>'
+    addListAppendItem += '<button class = "insertListItem" id = "+' + currentTable.is + '">+</button>';
+    addListAppendItem += '</td>'
+    for (let colIndex = 2; colIndex < colspan; colIndex++) {
+        addListAppendItem += '<td>';
+        addListAppendItem += '<input ';
+        addListAppendItem += 'id = "input' + currentTable.is + colIndex + '"';
+        addListAppendItem += 'type="text"';
+        addListAppendItem += ' placeholder="' + currentTable.columnNames[colIndex] + '"';
+        addListAppendItem += 'data-index="' + colIndex + '"';
+        addListAppendItem += '>'//endinput
+        addListAppendItem += '</td>';
+    }
+    addListAppendItem += '<td>'
+    addListAppendItem += '<button class = "cancelListItem"';
+    addListAppendItem += ' id = "cancel+' + currentTable.is + '">x</button>';
+    addListAppendItem += '</td>'
+    addListAppendItem += '</tr>'
+    $('#tbody' + currentTable.is).append(addListAppendItem)
+};
+
+function insertListItem() {
+    console.log('insertListItem()');
+    numberOfInputs = currentTable.columnNames.length
+    currentTable.newItem = [];
+    for (let inputIndex = 2; inputIndex < currentTable.columnNames.length; inputIndex++) {
+        var $currentInput = '#input'+currentTable.is+inputIndex;
+        currentTable.newItem.push($($currentInput).val());
+    }
+    console.log(currentTable.newItem)
+    $.ajax({
+        method: 'POST',
+        url: '/list',
+        data: currentTable,
+        success: function (response) {
+            console.log('response', response);
+            getTableData();
+        }
+    });
+}
+
+
+function cancelListItem () {
+    refreshBoxes();
+    getTableData();
+};
+
+function submitChecks () {
+    console.log('submitChecks()');
+
+};
--- server/routes/list.js
+++ server/routes/list.js
@@ -42,4 +42,78 @@ router.get('/', function (req, res) {
     });
 });
 
+router.delete('/:id', function (req, res) {
+    var listItemToRemove = req.params.id;
+    // Attempt to connect to database
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            // There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!!!
+            // Now, we're going to GET things from thd DB
+            client.query(`DELETE FROM ${currentTable.is} WHERE id=$1;`, [listItemToRemove], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    // Query failed. Did you test it in Postico?
+                    // Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(200);
+                }
+            });
+        }
+    });
+});
+
+
+router.post('/', function (req, res) {
+    var $tracker = 0;
+    currentTable = req.body
+    var sqlQueryString = '';
+    sqlQueryString += `INSERT INTO ${currentTable.is}` 
+    sqlQueryString += "(completion_status"
+    for (let index = 2; index < currentTable.columnNames.length; index++) {
+        sqlQueryString += `, ${currentTable.columnNames[index]}`;
+    };
+    sqlQueryString += ') '//end INSERT INTO
+    sqlQueryString += "VALUES ('n'";
+    for (let index = 0; index < currentTable.newItem.length; index++) {
+        $tracker++
+        sqlQueryString += ' , $'+$tracker;
+    };
+    sqlQueryString += ');';
+    console.log(sqlQueryString);
+
+    var sqlCleanserArray = [];
+    for (let index = 0; index < currentTable.newItem.length; index++) {
+        sqlCleanserArray.push(currentTable.newItem[index]);
+    };
+    console.log(sqlCleanserArray);
+    // Attempt to connect to database
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            // There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!!!
+            // Now, we're going to GET things from thd DB
+            client.query(sqlQueryString, sqlCleanserArray, function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    // Query failed. Did you test it in Postico?
+                    // Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+        }
+    });
+});
+
 module.exports = router;
